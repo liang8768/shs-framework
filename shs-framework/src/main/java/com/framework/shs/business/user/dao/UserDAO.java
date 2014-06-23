1f8d32d5ec2f260d53788ccfe5175717cf51a9e1
Liang <liangliang8768@qq.com>
2014-06-23 16:08:42 +0800

    修改userDAO位置

---
+++ shs-framework/src/main/java/com/framework/shs/business/user/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package com.framework.shs.business.user.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.framework.shs.business.entity.User;
+import com.framework.shs.core.dao.BaseDAO;
+
+/**
+ * 用户的数据访问类
+ * 
+ * UserDAO.java	
+ * @author Liang
+ * 2014年6月23日
+ */
+@Repository
+public class UserDAO extends BaseDAO<User>{
+}
