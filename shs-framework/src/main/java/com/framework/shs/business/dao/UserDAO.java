3229ea75994ff197b97dc173e8d502bfe99e9a4d
Liang <liangliang8768@qq.com>
2014-06-23 14:26:29 +0800

    改变userDAO的位置

---
+++ shs-framework/src/main/java/com/framework/shs/business/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package com.framework.shs.business.dao;
+
+import org.springframework.stereotype.Service;
+
+import com.framework.shs.business.entity.User;
+import com.framework.shs.core.dao.BaseDAO;
+
+/**
+ * 用户的数据访问类
+ * 
+ * UserDAO.java	
+ * @author Liang
+ * 2014年6月23日
+ */
+@Service
+public class UserDAO extends BaseDAO<User>{
+}
